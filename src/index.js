3e38876594d5c46c098858e38725a87ea31735b9
Minhao <924798079@qq.com>
2019-09-21 22:27:15 +0800

    尝试了devtool： source-map以及webpack-dev-server插件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash'
+import printMe from './print';
 
 function component() {
-    const element = document.createElement('div');
+    let element = document.createElement('div');
+    let btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     
+    btn.innerHTML = 'Click me and check console'
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
- };
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+                title: 'Outputsss Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+};
