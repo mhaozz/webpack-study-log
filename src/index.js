8de652d852c00e697bcd1e5c7c5d1c53f867bc7c
Minhao <924798079@qq.com>
2019-09-21 18:10:40 +0800

    回退准备

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import _ from 'lodash'
-import './style.css'
-import myimg from './my-image.jpg'
-import data from './data.xml'
 
 function component() {
     const element = document.createElement('div');
 
-    element.classList.add('hello')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     
-
-    let myImage = new Image();
-    myImage.src = myimg;
-    element.appendChild(myImage);
-
-    console.log(data);
-
     return element;
   }
   
