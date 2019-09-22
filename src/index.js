0a0ab77ea9e2f6d697aa3c3909869c2d40a70c60
Minhao <924798079@qq.com>
2019-09-22 19:33:28 +0800

    重建了起步一章的环境，便于后续调试

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-import _ from 'lodash'
-import printMe from './print';
-import './styles.css'
+import _ from 'lodash';
 
 function component() {
-    let element = document.createElement('div');
-    let btn = document.createElement('button');
-
+    var element = document.createElement('div');
+  
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    btn.innerHTML = 'Click me and check console'
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
+  
     return element;
   }
   
-  let element = component();
   document.body.appendChild(component());
-
-  if(module.hot) {
-    module.hot.accept('./print.js', () => {
-      console.log('Accepting the updated printMe module!');
-      document.body.removeChild(element);
-      element = component();
-      document.body.appendChild(element);
-    })
-  }
